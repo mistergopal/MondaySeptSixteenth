ce240b921dedd387941d0959d66700632d09a3db
Student <9thgrader@SMSHS-Tech20s-MacBook-Air.local>
2019-09-16 09:52:54 -0400

    Initial commit

---
+++ src/Today.java
@@ -0,0 +1,50 @@
+public class Today {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        System.out.println(myFirstNumber * 7);
+//        int v = 1;
+//        int k = 0;
+//        double h = 3.57;
+//
+//        if (k > h) {
+//            System.out.println("Yes");
+//        } else {
+//            System.out.println("No");
+//        }
+//        boolean isLeapYear;
+//
+//        int year = 1900;
+//
+//        if (year % 4 == 0) {
+//            if (year % 100 != 0) {
+//                isLeapYear = true;
+//            } else {
+//                if (year % 400 == 0) {
+//                    isLeapYear = true;
+//                } else {
+//                    isLeapYear = false;
+//                }
+//            }
+//        } else {
+//            isLeapYear = false;
+//        }
+//
+//        if ((year % 4 == 0) && (year % 100 != 0)) {
+//            isLeapYear = true;
+//        } else if (year % 400 == 0) {
+//            isLeapYear = true;
+//        } else {
+//            isLeapYear = false;
+//        }
+//
+//        if (!isLeapYear) {
+//            System.out.println("It's not a leap year");
+//        } else {
+//            System.out.println("It's a leap year");
+//        }
+
+
+    }
+}
